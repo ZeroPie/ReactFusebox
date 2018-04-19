27f7500cf059ca2d2532eb0b627e6bf62727bdb4
Pie <highsierrapie@Highs-iMac.fritz.box>
2018-04-19 13:31:22 +0200

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Component } from 'react';
+import './App.css';
+import * as logo from '../../logo.svg';
+import Main from '../Main/Main'
+import Header from '../Header/Header'
+
+const App = () => (
+    <div>
+        App.jsx
+        <Header />
+        <Main />
+    </div>
+  )
+  
+export default App
